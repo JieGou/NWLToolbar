7dc8f16d114b33e06b13a9709d6c23d390558c5d
kingbenhamin95 <ben@nwlarchitects.com>
2022-11-09 17:05:31 -0700

    got aligned notes working

--- NWLToolbar/AlignNotes.cs
+++ NWLToolbar/AlignNotes.cs
@@ -29,56 +29,70 @@ namespace NWLToolbar
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
+            //Grab Text Notes and Lines
             FilteredElementCollector textNotes = new FilteredElementCollector(doc, uidoc.Selection.GetElementIds())
                 .OfCategory(BuiltInCategory.OST_TextNotes)
                 .WhereElementIsNotElementType();
 
+            FilteredElementCollector keyNotes = new FilteredElementCollector(doc, uidoc.Selection.GetElementIds())
+                .OfCategory(BuiltInCategory.OST_KeynoteTags)
+                .WhereElementIsNotElementType();
 
-            
             FilteredElementCollector lines = new FilteredElementCollector(doc, uidoc.Selection.GetElementIds())
                 .OfClass(typeof(CurveElement))
                 .OfCategory(BuiltInCategory.OST_Lines)
                 .WhereElementIsNotElementType();
-            
-                        
-            
-            
-            
+
+            FilteredElementCollector mLines = new FilteredElementCollector(doc, uidoc.Selection.GetElementIds())
+                .OfClass(typeof(CurveElement))
+                .OfCategory(BuiltInCategory.OST_GenericLines)
+                .WhereElementIsNotElementType();
+
+            //Variables
+            XYZ lineX = new XYZ(0, 0, 0);
+
             //Transaction Start
             Transaction t = new Transaction(doc);
             t.Start("Align Notes");
 
-            XYZ lineX = new XYZ(0, 0, 0);
-
-            foreach (DetailLine l in lines)
+            //Grab Line Origin and Set X Coordinate
+            foreach (DetailLine dl in lines)
             {
 
-                Line line = l.GeometryCurve as Line;
-                                
-                lineX = new XYZ(line.Origin.X, 0, 0);
+                Line line = dl.GeometryCurve as Line;
 
+                lineX = new XYZ(line.Origin.X, 0, 0);
                 break;
 
             }
-            TaskDialog.Show("title", "new Point is " + lineX.ToString());
-
-
+           
+            //Grab each text note and set new location
             foreach (TextNote e in textNotes)
             {
-
-                XYZ lp = e.Coord as XYZ;
                 
+                XYZ origLocation = e.Coord as XYZ;               
+                                
+                XYZ newLocation = new XYZ(lineX.X, origLocation.Y, 0);                
                 
-                ElementId id = e.Id;
+                XYZ offset = origLocation - newLocation;
 
-                XYZ newLocation = new XYZ(lineX.X, lp.Y, lp.Z);
-                
-
-                ElementTransformUtils.MoveElement(doc, id, newLocation);                           
-                               
+                ElementTransformUtils.MoveElement(doc, e.Id, -offset);                         
 
             }
             
+            //Grab each text note and set new location
+            foreach (IndependentTag e in keyNotes)
+            {
+                
+                XYZ origLocation = e.TagHeadPosition as XYZ;                
+                
+                XYZ newLocation = new XYZ(lineX.X, origLocation.Y, 0);                
+
+                XYZ offset = origLocation - newLocation;                
+
+                ElementTransformUtils.MoveElement(doc, e.Id, -offset);
+
+            }
 
             //Finish Transaction
             t.Commit();
